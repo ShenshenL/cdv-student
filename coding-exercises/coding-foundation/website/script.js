177c02ba753bdf345c475d1a8aec7a1c42eda3d1
ShenshenL <sl6899@nyu.edu>
2021-01-26 19:13:40 +0800

    assignment

--- coding-exercises/coding-foundation/website/script.js
+++ coding-exercises/coding-foundation/website/script.js
@@ -1,1 +1,16 @@
-console.log(111)
+
+function get(){
+  document.getElementById('x').innerHTML = "";
+  var num=document.getElementById('data')
+
+  // alert(num.value);
+  for (i = 0; i < num.value; i++) {
+    var img = document.createElement("img");
+    img.src = "tree.png";
+    img.height=50
+    img.width=50
+    var src = document.getElementById("x");
+
+    src.append(img);
+  }
+}
