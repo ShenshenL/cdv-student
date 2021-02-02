bc03a198c4f476addb46d6f93ac1fe103583f344
ShenshenL <sl6899@nyu.edu>
2021-02-03 00:13:02 +0800

    e2

---
+++ coding-exercises/placeholder/exercise2/data.js
@@ -0,0 +1,202 @@
+newData=[
+    {
+        "avg": 5,
+        "slg": 2,
+        "rts": 5,
+        "puzzleGame": 3,
+        "boardGame": 2,
+        "moba": 5,
+        "musicGame": 4,
+        "undefined": 4
+    },
+    {
+        "avg": 4,
+        "slg": 3,
+        "rts": 4,
+        "puzzleGame": 3,
+        "boardGame": 2,
+        "moba": 3,
+        "musicGame": 3,
+        "undefined": 2
+    },
+    {
+        "avg": 3,
+        "slg": 1,
+        "rts": 2,
+        "puzzleGame": 2,
+        "boardGame": 2,
+        "moba": 5,
+        "musicGame": 5,
+        "undefined": 4
+    },
+    {
+        "avg": 2,
+        "slg": 5,
+        "rts": 5,
+        "puzzleGame": 3,
+        "boardGame": 4,
+        "moba": 5,
+        "musicGame": 5,
+        "undefined": 5
+    },
+    {
+        "avg": 4,
+        "slg": 2,
+        "rts": 3,
+        "puzzleGame": 1,
+        "boardGame": 2,
+        "moba": 4,
+        "musicGame": 5,
+        "undefined": 5
+    },
+    {
+        "avg": 5,
+        "slg": 5,
+        "rts": 5,
+        "puzzleGame": 4,
+        "boardGame": 2,
+        "moba": 3,
+        "musicGame": 5,
+        "undefined": 5
+    },
+    {
+        "avg": 3,
+        "slg": 2,
+        "rts": 5,
+        "puzzleGame": 3,
+        "boardGame": 5,
+        "moba": 4,
+        "musicGame": 3,
+        "undefined": 4
+    },
+    {
+        "avg": 5,
+        "slg": 5,
+        "rts": 5,
+        "puzzleGame": 3,
+        "boardGame": 5,
+        "moba": 5,
+        "musicGame": 4,
+        "undefined": 3
+    },
+    {
+        "avg": 1,
+        "slg": 1,
+        "rts": 2,
+        "puzzleGame": 5,
+        "boardGame": 1,
+        "moba": 4,
+        "musicGame": 5,
+        "undefined": 5
+    },
+    {
+        "avg": 5,
+        "slg": 5,
+        "rts": 5,
+        "puzzleGame": 3,
+        "boardGame": 2,
+        "moba": 4,
+        "musicGame": 4,
+        "undefined": 4
+    },
+    {
+        "avg": 1,
+        "slg": 5,
+        "rts": 5,
+        "puzzleGame": 4,
+        "boardGame": 3,
+        "moba": 5,
+        "musicGame": 2,
+        "undefined": 4
+    },
+    {
+        "avg": 1,
+        "slg": 4,
+        "rts": 3,
+        "puzzleGame": 1,
+        "boardGame": 2,
+        "moba": 4,
+        "musicGame": 3,
+        "undefined": 2
+    },
+    {
+        "avg": 5,
+        "slg": 4,
+        "rts": 5,
+        "puzzleGame": 2,
+        "boardGame": 1,
+        "moba": 4,
+        "musicGame": 4,
+        "undefined": 5
+    },
+    {
+        "avg": 1,
+        "slg": 1,
+        "rts": 2,
+        "puzzleGame": 1,
+        "boardGame": 1,
+        "moba": 3,
+        "musicGame": 4,
+        "undefined": 1
+    },
+    {
+        "avg": 1,
+        "slg": 1,
+        "rts": 2,
+        "puzzleGame": 1,
+        "boardGame": 1,
+        "moba": 1,
+        "musicGame": 5,
+        "undefined": 2
+    },
+    {
+        "avg": 4,
+        "slg": 3,
+        "rts": 4,
+        "puzzleGame": 3,
+        "boardGame": 2,
+        "moba": 3,
+        "musicGame": 3,
+        "undefined": 2
+    },
+    {
+        "avg": 3,
+        "slg": 2,
+        "rts": 3,
+        "puzzleGame": 3,
+        "boardGame": 3,
+        "moba": 4,
+        "musicGame": 4,
+        "undefined": 2
+    },
+    {
+        "avg": 4,
+        "slg": 4,
+        "rts": 3,
+        "puzzleGame": 3,
+        "boardGame": 4,
+        "moba": 3,
+        "musicGame": 3,
+        "undefined": 5
+    },
+    {
+        "avg": 1,
+        "slg": 1,
+        "rts": 1,
+        "puzzleGame": 1,
+        "boardGame": 1,
+        "moba": 2,
+        "musicGame": 2,
+        "undefined": 4
+    },
+    {
+        "avg": 1,
+        "slg": 5,
+        "rts": 5,
+        "puzzleGame": 1,
+        "boardGame": 1,
+        "moba": 1,
+        "musicGame": 1,
+        "undefined": 3
+    }
+]
---
+++ coding-exercises/placeholder/exercise2/script.js
@@ -0,0 +1,79 @@
+
+// the function dates a data
+// arrayn as an argument
+function averageData(data){
+  // new empty array to be filled
+  // with data in new structure
+  let newData = [];
+  // assuming each data point has the same
+  // keys/categories, we extract an array of them from the
+  // first data point in the array
+  // in class we changed it to the last element instead
+  // as the first one did not have all the categories filled out
+  // there is more thorough ways to do this, but for out purposes
+  // now, this will be enough
+  let keys = Object.keys(data[0]);
+  // now we loop over the keys/categories
+  for(let i = 0; i < keys.length; i++){
+    // store the current key/category in
+    // a variable:
+    let key = keys[i];
+    // now we will loop over each data point
+    // in the data set, check if it has a value
+    // for the key/category and add them to
+    // a total sum variable
+    // as well as count the occurences in order to
+    // calulate the averae in the end
+    let sum = 0;
+    let num = 0;
+    for(let j = 0; j < data.length; j++){
+      let datum = data[j];
+      // check if the key exists
+      // for this datapoint
+      if(key in datum){
+        // add to sum
+        sum += datum[key];
+        // increase count
+        num++;
+      }
+    }
+    // now calculate the average
+    let avg = sum/num;
+    // make sure the value is a number
+    // (some value might be strings)
+    if(!isNaN(avg)){
+      // create an object with both the average
+      // and also the number of measurements that
+      // went into the average
+      let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
+      // add the new datapoint to the new data array
+      newData.push(newDataPoint);
+    }
+  }
+  // return everything when it is done
+  return newData;
+}
+
+let transformedData = averageData(newData);
+let chart = document.getElementById('chart');
+let speed=3;
+
+console.log(transformedData);
+
+
+for (i=0; i< transformedData.length; i++) {
+  let bar = document.createElement('div');
+  let space = document.createElement('div');
+  bar.className = 'bar ';
+  bar.innerHTML = transformedData[i].name+'   '+transformedData[i].average.toFixed(2);
+
+  bar.style.width = transformedData[i].average*80 + 'px';
+  bar.style.height = 50 +  'px'
+  bar.style.backgroundColor= "#7373B9"
+  space.style.height=20 + 'px'
+
+
+  chart.appendChild(bar);
+  chart.appendChild(space)
+  console.log(bar.style.width);
+}
